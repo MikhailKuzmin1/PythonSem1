229b0682b3697d6a97799649bc3af4b51ecc7e10
Mikhail <mikyzmin97@gmail.com>
2023-07-04 22:49:52 +0300

    Change Name

--- task_4.py
+++ task_4.py
@@ -1,8 +1,8 @@
-haigth = int(input('Введите колличество долек в длинну: '))
+length = int(input('Введите колличество долек в длинну: '))
 width = int(input('Введите колличество долек в ширину: '))
 chocolate = int(input('Введите колличество долек, которое хотите отломить: '))
-S = haigth*width
-if chocolate < S  and (chocolate%width == 0 or chocolate%haigth == 0):
+S = length*width
+if chocolate < S  and (chocolate%width == 0 or chocolate%length == 0):
     print('Можно')
 else:
     print('Нельзя')  
